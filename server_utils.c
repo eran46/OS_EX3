78a5cd400bd221c59e92bb91e7de9a7654f56285
eran46 <eran.hir@gmail.com>
2024-12-31 20:35:08 +0200

    removed unnecessary folders
    and finished server functionlaity

--- server_utils.c
+++ server_utils.c
@@ -1,11 +1,9 @@
+#include "server_utils.h"
 #include <sys/socket.h>
+#include <stdio.h>
 
-int get_port(char* argv[]){
 
-    return str_to_int(char* argv[1]);
-}
-
-void check_arg_num(int argc){
+int incorrect_arg_num(int argc){
     if(argc < 2){
     	printf("Wrong number of arguments. (missing port)");
     	return 1;
@@ -14,13 +12,12 @@ void check_arg_num(int argc){
     	printf("Wrong number of arguments. (too many arguments)");
     	return 1;
     }
+    return 0;
 }
 
 void free_client_mem(Client* client){
 	free(client.client_address);
-	free(client.client_socket);
 	free(client.client_name);
-	free(client.client_index);
 	free(client);
 }
 
@@ -32,31 +29,12 @@ void init_server_socket(int port){
     return server_socket;
 }
 
-struct sockaddr_in init_server_address(port){
-    struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address)); // prevent garbage values    
-    server_address.sin_family=AF_INET; // IPv4
-    server_address.sin_port = htons(port); // port number - ensure Big Endian
-    server_address.sin_addr = INADDR_ANY; // (struct in_addr) accept any IPv4 address
-    return server_address;
-}
-
 int bind_socket_to_addr(int socket,struct sockaddr_in *server_address){
     if(bind(socket, (struct sockaddr_in*)server_address, sizeof(*server_address) == -1){ // bind failed
     	printf("binding socket to server address failed");
     	return -1;
     }
     return 0;
-}
-
-void reap_zombies(int sig) {
-    (void)sig; // stop unused parameter warning
-    int waitpid_ret_status;
-    
-    while ((waitpid_ret_status = waitpid(-1, NULL, WNOHANG)) > 0); // reap all current zombie proccesses
-    if((waitpid_ret_status == -1 && errno != ECHILD){ // error on waitpid and not error-from-no-waiting-zombie-process
-                printf("failed to reap zombie client process. (waitpid())\n");
-    }
 }
 
 void init_clients(Client* clients){
@@ -68,7 +46,7 @@ void init_clients(Client* clients){
 // Function to check if the clients array is full
 int is_clients_full() {
     for (int i = 0; i < MAX_CONNECTED_CLIENTS; i++) {
-        if (clients[i]->client_socket == 0) {  // Socket 0 indicates unused slot
+        if (clients[i] == NULL) {  // NULL indicates unused slot
             return 0;  // Not full
         }
     }
@@ -96,8 +74,8 @@ void remove_client(int index) {
 	close(clients[index]->client_socket); // close client socket
 	free_client_mem(clients[index])// free dynamically allocated memory
 	clients[index] = NULL;
-	pthread_mutex_unlock(&clients_mutex);
 	pthread_cond_signal(&clients_cond);
+	pthread_mutex_unlock(&clients_mutex);
 }
 
 // Helper function to get the index of a client in the clients array
@@ -148,4 +126,20 @@ int find_client_index_by_name(char* name) {
     }
     return -1;  // client not found
 }
+
+void echo_msg_to_all_clients(Client* client, char* out_message){
+	pthread_mutex_lock(&clients_mutex);  // accessing clients - critical region
+	for (int i = 0; i < MAX_CONNECTED_CLIENTS; i++){
+		if(clients[i] != NULL){
+			if (send(clients[i]->client_socket, out_message, strlen(out_message), 0) < 0) {
+				printf("failed while sending message from %s to all client. (send())\n", clients[i].client_name);
+			}
+		}
+	}
+	pthread_mutex_unlock(&clients_mutex);
+}
+
+
+
+
 
